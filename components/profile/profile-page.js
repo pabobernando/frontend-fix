931c10294ba8cd28c14cbf25d4423ef2d7343a56
pabobernando <pabobernando69@gmail.com>
2022-03-17 03:43:36 +0700

    apes everyday

--- components/profile/profile-page.js
+++ components/profile/profile-page.js
@@ -1,67 +1,93 @@
-import React, { useEffect, useState }  from 'react';
-import Image from 'next/image'
-import Link from 'next/link'
-import ProfilePic from '../../public/assets/avatar.png'
-import axios from 'axios';
-import jsCookie from 'js-cookie';
-
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import ProfilePic from "../../public/assets/avatar.png";
+import axios from "axios";
+import jsCookie from "js-cookie";
 
 function ProfilePage() {
-
-  const [user, setUser] = useState({})
+  const [user, setUser] = useState({});
 
   useEffect(() => {
     async function fetchData() {
-      const token = jsCookie.get('token')
+      const token = jsCookie.get("token");
 
       const myToken = axios.create({
-        baseURL: 'http://localhost:3001',
+        baseURL: "http://localhost:3001",
         timeout: 100000,
-        headers: {'Authorization': 'Bearer '+token}
+        headers: { Authorization: "Bearer " + token },
       });
-      
-      const response = await myToken.get('/userGames/me')
-      setUser(response.data)
+
+      const response = await myToken.get("/userGames/me");
+      setUser(response.data);
     }
-    fetchData()
-  }, [])
+    fetchData();
+  }, []);
+
+  // gambar
+  const [image, setImage] = useState([]);
+  const [saveImage, setSaveImage] = useState(null);
+
+  function handleUploadChange(e) {
+    console.log(e.target.files[0]);
+    let uploaded = e.target.files[0];
+    setImage(URL.createObjectURL(uploaded));
+  }
+
   return (
     <div className="container">
-  <div className="row ">
+      <div className="row ">
+        <div className="col-6">
+          <div className="container pt-5 pb-5">
+            <div className="row ">
+              <div className="col-md-4 bg-secondary p-1">
+                <Image
+                  src={ProfilePic}
+                  className="img-thumbnail"
+                  width={200}
+                  height={150}
+                />
+              </div>
 
-    <div className="col-6">
-    <div className="container pt-5 pb-5">
-  <div className="row ">
+              <div className="col-md-8 bg-secondary text-center">
+                <h2>{user.userName}</h2>
+                <p>{user._id}</p>
+                <button type="button" className="btn btn-primary">
+                  <Link href="profile/edit">
+                    <a className="text-decoration-none text-light">
+                      Edit Profile
+                    </a>
+                  </Link>
+                </button>
+              </div>
+              <div className="col-4 text-center">
+                <label htmlFor="formfile" className="form-label">
+                  Upload Image Here
+                </label>
+                <input
+                  type="file"
+                  className="form-control"
+                  id="formFile"
+                  onChange={handleUploadChange}
+                  accept="image/*"
+                />
+                <button type="button" className="btn btn-dark mt-1">
+                  Save my Photo
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
 
-    <div className="col-md-4 bg-secondary rounded border border-dark p-3">
-<Image src={ProfilePic}  width={100}  height={100} />
+        <div className="col-12 border border-dark pt-1 pb-1">
+          <div className="col-8 ">
+            <p>Experience : {user.experience}</p>
+            <p>Level : {user.lvl}</p>
+            <p>Total Score : {user.totalScore}</p>
+          </div>
+        </div>
+      </div>
     </div>
-
-    <div className="col-md-8 bg-secondary rounded border border-dark p-3">
-      
-<h2>{ user.userName }</h2>
-<p>{ user._id }</p>
-<button type="button" className="btn btn-primary"><Link href="profile/edit">
-          <a className='text-decoration-none text-light'>Edit Profile</a>
-        </Link></button>
-        
-    </div>
-
-  </div>
-</div>
-    </div>
-
-    <div className="col-12 border border-dark pt-1 pb-1"> 
-
-    <div className="col-8 ">
-<p>Experience : {user.experience}</p>
-<p>Level : {user.lvl}</p>
-<p>Total Score : {user.totalScore}</p>
-    </div>
-    </div>
-
-  </div>
-</div>
   );
 }
 
