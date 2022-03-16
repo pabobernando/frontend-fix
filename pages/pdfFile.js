1c0475725ce93d724cd7454ede0608672b934739
pabobernando <pabobernando69@gmail.com>
2022-03-16 18:05:06 +0700

    pdf sukses

--- pages/pdfFile.js
+++ pages/pdfFile.js
@@ -1,21 +1,14 @@
-import { PDFDownloadLink } from "@react-pdf/renderer";
 import React from "react";
 import PdfGame from "../components/game/pdfGame";
-import classes from "./pdfFile.module.css";
 
 function PdfFile() {
+  function handlePrint() {
+    window.print();
+  }
   return (
     <div>
       <PdfGame />
-      <PDFDownloadLink document={<PdfGame />} fileName="FORM">
-        {({ loading }) =>
-          loading ? (
-            <button className={classes.button}>Loading Document...</button>
-          ) : (
-            <button className={classes.button}>Download</button>
-          )
-        }
-      </PDFDownloadLink>
+      <button onClick={handlePrint}>Print</button>
     </div>
   );
 }
